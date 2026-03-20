72fa19c341996abbf48e8b2cd03f8e23643f2762
Chivang XIANENG <chivang.x@namngiep1.local>
2026-03-20 16:04:49 +0700

    Fix change font subset to lao

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 // ຕັ້ງຄ່າຟອນ Phetsarath
 const phetsarath = Phetsarath({
   weight: ["400", "700"], // ໂຫລດທັງແບບຕົວທຳມະດາ ແລະ ຕົວເຂັ້ມ
-  subsets: ["latin"],     // ມາດຕະຖານ Next.js
+  subsets: ["lao"],     // ມາດຕະຖານ Next.js
   display: "swap",        // ຊ່ວຍໃຫ້ຕົວໜັງສືຂຶ້ນໄວ ໂດຍບໍ່ຕ້ອງລໍຖ້າຟອນໂຫລດດົນ
 });
 
